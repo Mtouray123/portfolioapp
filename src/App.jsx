bc207b67c7dec36a8018d19d038d84b3e91b20e5
Mahamadou Touray <mahamadou12@gmail.com>
2023-03-29 09:49:00 -0400

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import React from 'react';
+import Header from './components/header/Header';
+import NavBar from './components/nav/NavBar';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
+import Services from './components/services/Services';
+import Contact from './components/contact/Contact';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <NavBar />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
 
     </div>
   );
