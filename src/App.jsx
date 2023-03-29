83a1f57e6b67d33840b2f091320ca8f8f190aec9
Mahamadou Touray <mahamadou12@gmail.com>
2023-03-29 10:42:55 -0400

    dark-mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import NavBar from './components/nav/NavBar';
 import About from './components/about/About';
 import Experience from './components/experience/Experience';
 import Footer from './components/footer/Footer';
-import Services from './components/services/Services';
 import Contact from './components/contact/Contact';
 import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonials';
 
 import './App.css';
 
@@ -18,9 +16,7 @@ function App() {
       <NavBar />
       <About />
       <Experience />
-      <Services />
       <Portfolio />
-      <Testimonials />
       <Contact />
       <Footer />
 
